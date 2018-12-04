b443a93eaf9dfbed719329307083c5fbca74a551
Reza Zahedi <reza.b.e@hotmail.com>
2018-12-04 22:07:45 +0330

    changin to simple linprog funtion

--- A_finalFun.m
+++ A_finalFun.m
@@ -16,14 +16,14 @@ for n = 1:N
                     colNum = n + (m-1)*N + (k-1)*N*M + (u-1)*K*N*M + ...
                         + (t-1)*U*K*N*M;2;
                     A(rowNum, colNum) = 1;
-                    A(rowNum + 1, colNum) = -1;
+%                     A(rowNum + 1, colNum) = -1;
                     
                     if t > deltaT(m, n)
                         % "x_m,n"
                         colNum = m + (n-1)*M + (k-1)*N*M + (u-1)*K*N*M + ...
                             + (t - deltaT(m, n) - 1)*U*K*N*M;
                         A(rowNum, colNum) = -1;
-                        A(rowNum + 1, colNum) = 1;
+%                         A(rowNum + 1, colNum) = 1;
                     end % end of "x" coef.
                     
                     for u_p = 1:U_P % assigning "E" coef.
@@ -32,7 +32,7 @@ for n = 1:N
                             + (u-1)*K*N*M + (u_p-1)*U*K*N*M +...
                             + (t-1)*U_P*U*K*N*M;
                         A(rowNum, colNum) = 1;
-                        A(rowNum + 1, colNum) = -1;
+%                         A(rowNum + 1, colNum) = -1;
                         
                         if t > (deltaT(m, n) + 2)
                             % "E_m,n"
@@ -41,7 +41,7 @@ for n = 1:N
                                 + (u_p-1)*U*K*N*M + ...
                                 + (t - deltaT(m, n) - 2 - 1)*U_P*U*K*N*M;
                             A(rowNum, colNum) = -1;
-                            A(rowNum + 1, colNum) = 1;
+%                             A(rowNum + 1, colNum) = 1;
                         end
                     end % end of "E" coef.
                 end
@@ -49,16 +49,17 @@ for n = 1:N
                 colNum = T*U*K*N*M*(2+U_P) + n + (k-1)*N + (u-1)*K*N + ...
                     + (t-1)*U*K*N;
                 A(rowNum, colNum) = 1;
-                A(rowNum + 1, colNum) = -1;
+%                 A(rowNum + 1, colNum) = -1;
                 
                 if t > 1
                     colNum = T*U*K*N*M*(2+U_P) + n + (k-1)*N +...
                         + (u-1)*K*N + (t - 1 - 1)*U*K*N;
                     A(rowNum, colNum) = -1;
-                    A(rowNum + 1, colNum) = 1;
+%                     A(rowNum + 1, colNum) = 1;
                 end % end of "i" coef.
                 
-                rowNum = rowNum + 2;   
+%                 rowNum = rowNum + 2;   
+                rowNum = rowNum + 1;   
             end
         end
     end
@@ -81,25 +82,25 @@ for m = 1:M
                         colNum = T*U*K*N*M +  m + (n-1)*M + (k-1)*N*M + ...
                             + (u-1)*K*N*M + (u_p-1)*U*K*N*M +...
                             + (t-1)*U_P*U*K*N*M;
-%                         A(rowNum, colNum) = 1;
+                        A(rowNum, colNum) = 1;
 %                         A(rowNum + 1, colNum) = -1;
-                        A(rowNum, colNum) = -1;
+%                         A(rowNum, colNum) = -1;
                     end
                     % end of "E" coef.
                     
                     % assigning "W" coef.
                     colNum = T*U*K*N*M*(1+U_P) + m + (n-1)*M + ...
                         + (k-1)*N*M + (u-1)*K*N*M + (t-1)*U*K*N*M;
-%                     A(rowNum, colNum) = 1;
+                    A(rowNum, colNum) = 1;
 %                     A(rowNum + 1, colNum) = -1;
-                    A(rowNum, colNum) = -1;
+%                     A(rowNum, colNum) = -1;
                     
                     if t > 1
                         colNum = T*U*K*N*M*(1+U_P) + m + (n-1)*M +...
                             + (k-1)*N*M + (u-1)*K*N*M + (t - 1 - 1)*U*K*N*M;
-%                         A(rowNum, colNum) = -1;
+                        A(rowNum, colNum) = -1;
 %                         A(rowNum + 1, colNum) = 1;
-                        A(rowNum, colNum) = 1;
+%                         A(rowNum, colNum) = 1;
                     end
                     % end of "W" coef.
 %                     rowNum = rowNum + 2;
@@ -111,41 +112,41 @@ for m = 1:M
 end
 
 %--------------------------------------------------------------------------
-%% forth set of constraints:
-for k = 1:K
-    for u = 1:U
-        for n = 1:N
-            % assigning "i" coef.
-            colNum = T*U*K*N*M*(2+U_P) + n + (k-1)*N + (u-1)*K*N +...
-                + (T-1)*U*K*N;
-            A(rowNum, colNum) = 1;
-            A(rowNum + 1, colNum) = -1;
-            % end of "i" coef.
-            
-            for m = 1:M
-                % assigning "W" coef.
-                colNum = T*U*K*N*M*(1+U_P) + m + (n-1)*M + (k-1)*N*M + ...
-                    + (u-1)*K*N*M + (T-1)*U*K*N*M;
-                A(rowNum, colNum) = 1;
-                A(rowNum + 1, colNum) = -1;
-                % end of "W" coef.
-                
-                for u_p = 1:U_P
-                    for t = 1:T
-                        % assigning "E" coef.
-                        colNum = T*U*K*N*M +  m + (n-1)*M + (k-1)*N*M + ...
-                            + (u-1)*K*N*M + (u_p-1)*U*K*N*M + ...
-                            + (t-1)*U_P*U*K*N*M;
-                        A(rowNum, colNum) = 1;
-                        A(rowNum + 1, colNum) = -1;
-                        % end of "E" coef.
-                    end
-                end
-            end
-        end
-        
-        rowNum = rowNum + 2;
-    end
-end
-
+% %% forth set of constraints:
+% for k = 1:K
+%     for u = 1:U
+%         for n = 1:N
+%             % assigning "i" coef.
+%             colNum = T*U*K*N*M*(2+U_P) + n + (k-1)*N + (u-1)*K*N +...
+%                 + (T-1)*U*K*N;
+%             A(rowNum, colNum) = -1;
+%             A(rowNum + 1, colNum) = 1;
+%             % end of "i" coef.
+%             
+%             for m = 1:M
+%                 % assigning "W" coef.
+%                 colNum = T*U*K*N*M*(1+U_P) + m + (n-1)*M + (k-1)*N*M + ...
+%                     + (u-1)*K*N*M + (T-1)*U*K*N*M;
+%                 A(rowNum, colNum) = 1;
+%                 A(rowNum + 1, colNum) = -1;
+%                 % end of "W" coef.
+%                 
+%                 for u_p = 1:U_P
+%                     for t = 1:T
+%                         % assigning "E" coef.
+%                         colNum = T*U*K*N*M +  m + (n-1)*M + (k-1)*N*M + ...
+%                             + (u-1)*K*N*M + (u_p-1)*U*K*N*M + ...
+%                             + (t-1)*U_P*U*K*N*M;
+%                         A(rowNum, colNum) = 1;
+%                         A(rowNum + 1, colNum) = -1;
+%                         % end of "E" coef.
+%                     end
+%                 end
+%             end
+%         end
+%         
+%         rowNum = rowNum + 2;
+%     end
+% end
+% 
 end
